f354d3d8728cbdfe47ad8a9440904d2390578c0d
Poonam7505 <Poonam82872@gmail.com>
2022-04-05 12:23:16 +0530

    introducing quiz

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Navbar from './Navbar';
+import hat from '../assets/images/sortinghat.gif';
+import  '../css/StyleSortinghat.css'
 
 export default function About() {
   return (
-    <div>
+    <div className='box'>
     <Navbar/>
+    <div className='quizbox'>
+    <div className='imgbox'>
+<img src={hat} alt=""/>
+    </div>
+    <div>
+<h2>You will be sorted in your house on the basis of quiz</h2>
+<button className='quizbtn'>Start</button>
+    </div>
+    </div>
     </div>
   )
 }
